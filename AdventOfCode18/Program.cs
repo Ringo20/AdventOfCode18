0648d5882b65453af8b48675745dcc15a83f090a
Ringo20 <lorenzo.curzi20@gmail.com>
2018-12-05 22:32:50 +0100

    Add project files.

---
+++ AdventOfCode18/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
